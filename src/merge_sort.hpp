215675a7f51575e45cbeae2758cdaea2c0898cc5
Daniel Kiss <littlesnorrboy@gmail.com>
2018-09-07 00:29:14 +0200

    Refactor merge_sort

--- src/merge_sort.hpp
+++ src/merge_sort.hpp
@@ -5,81 +5,67 @@
 
 namespace merge_sort {
 
+/**
+ * Sort a range via the merge sort algorithm, It needs to be a random access
+ * iterator
+ */
 template <typename It>
-class MergeSortImpl;
+void sort(It begin, It end);
 
 template <typename It>
-void sort(It begin, It end)
-{
+void sort_impl(It begin, It end, std::random_access_iterator_tag);
+
+template <typename It>
+void top_down_split_merge(It B, size_t begin, size_t end, It A);
+
+template <typename It>
+void merge(It A, size_t begin, size_t mid, size_t end, It B);
+
+template <typename It>
+void sort(It begin, It end) {
     using category = typename std::iterator_traits<It>::iterator_category;
-    auto sorter = MergeSortImpl<It> { begin, end, category() };
-    sorter.sort();
+    sort_impl(begin, end, category());
 }
 
 template <typename It>
-class MergeSortImpl {
+void sort_impl(It begin, It end, std::random_access_iterator_tag) {
     using TValue = typename std::iterator_traits<It>::value_type;
-    using Container = std::vector<TValue>;
+    const auto size = end - begin;
 
-    It begin;
-    It end;
-    Container A;
-    Container B;
+    std::vector<TValue> working_container;
+    working_container.reserve(size);
+    std::copy(begin, end, std::back_inserter(working_container));
 
-public:
-    MergeSortImpl(It begin, It end, std::random_access_iterator_tag)
-        : begin(begin)
-        , end(end)
-    {
+    top_down_split_merge(working_container.begin(), 0, size, begin);
+}
+
+template <typename It>
+void top_down_split_merge(It B, size_t begin, size_t end, It A) {
+    // If length is less than 2 consider the range sorted
+    if (end - begin < 2) {
+        return;
     }
 
-    void sort()
-    {
-        const auto size = end - begin;
+    auto mid = (end + begin) / 2;
 
-        auto init = [&](auto& container) {
-            container.reserve(size);
-            std::copy(begin, end, std::back_inserter(container));
-        };
+    top_down_split_merge(A, begin, mid, B);
+    top_down_split_merge(A, mid, end, B);
 
-        init(A);
-        init(B);
+    merge(B, begin, mid, end, A);
+}
 
-        top_down_split_merge(B, 0, size, A);
-
-        // Copy the result back to the original range
-        std::copy(A.begin(), A.end(), begin);
-    }
-
-private:
-    static void top_down_split_merge(Container& B, size_t begin, size_t end, Container& A)
-    {
-        // If length is less than 2 consider the range sorted
-        if (end - begin < 2)
-            return;
-
-        auto mid = (end + begin) / 2;
-
-        top_down_split_merge(A, begin, mid, B);
-        top_down_split_merge(A, mid, end, B);
-
-        merge(B, begin, mid, end, A);
-    }
-
-    static void merge(Container& A, size_t begin, size_t mid, size_t end, Container& B)
-    {
-        auto i = begin, j = mid;
-        for (; begin < end; ++begin) {
-            if (i < mid && (j >= end || A[i] <= A[j])) {
-                B[begin] = A[i];
-                ++i;
-            } else {
-                B[begin] = A[j];
-                ++j;
-            }
+template <typename It>
+void merge(It A, size_t begin, size_t mid, size_t end, It B) {
+    auto i = begin, j = mid;
+    for (; begin < end; ++begin) {
+        if (i < mid && (j >= end || *(A + i) <= *(A + j))) {
+            *(B + begin) = *(A + i);
+            ++i;
+        } else {
+            *(B + begin) = *(A + j);
+            ++j;
         }
     }
-};
+}
 
-} // namespace merge_sort
-
+}  // namespace merge_sort
